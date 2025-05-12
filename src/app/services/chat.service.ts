0346e8aefb6e14a5296b554486121b3797fa4cd8
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-12 16:25:32 +0200

    refactor: update message loading in chat component and adjust API URL in chat service

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -2,32 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '../models/message.model';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ChatService {
-    private apiUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:Y6FZ87f5';
+    private API_URL = 'https://x8ki-letl-twmt.n7.xano.io/api:DzvuDCJG/messages';
 
     constructor(
         private http: HttpClient,
         private authService: AuthService
     ) {}
 
-    getMessages(): Message[] {
-        return [
-            {
-                id: 1,
-                content: 'Hello, how are you?',
-                senderId: 1,
-                createdAt: new Date(),
-            },
-            {
-                id: 2,
-                content: 'I am fine, thank you!',
-                senderId: 2,
-                createdAt: new Date(),
-            },
-        ];
+    getMessages(): Observable<Message[]> {
+        return this.http.get<Message[]>(this.API_URL);
     }
 }
