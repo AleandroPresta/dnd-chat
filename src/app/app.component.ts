8bb70134f742a10da35e0cc48c5123004cb438af
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-14 12:53:45 +0200

    Added auth guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ChatComponent } from './components/chat/chat.component';
-import { SigninComponent } from './components/signin/signin.component';
-import { NgIf } from '@angular/common';
-import { User } from './models/user.model';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [SigninComponent, ChatComponent, NgIf],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    componentToShow: 'signin' | 'chat' = 'signin'; // signin is now default
-    currentUser: User = {
-        id: 1,
-        first_name: 'Alice',
-        last_name: 'Smith',
-        email: 'alice@smith.com',
-    };
-
-    ngOnInit(): void {
-        // You can add logic here if needed
-    }
-
-    onAuthenticated(): void {
-        this.componentToShow = 'chat';
-    }
-
-    onLogout(): void {
-        this.componentToShow = 'signin';
-    }
-}
+export class AppComponent {}
