9d3c4c174ba408b02f4594cd5ba91f3c358632d7
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-05-14 17:13:47 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/components/chat/chat-header/chat-header.component.ts
+++ src/app/components/chat/chat-header/chat-header.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { getAuth, signOut } from 'firebase/auth';
 
 @Component({
-  selector: 'app-chat-header',
-  imports: [],
-  templateUrl: './chat-header.component.html',
-  styleUrl: './chat-header.component.scss'
+    selector: 'app-chat-header',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './chat-header.component.html',
+    styleUrl: './chat-header.component.scss',
 })
 export class ChatHeaderComponent {
+    @Output() logoutEvent = new EventEmitter<void>();
 
+    constructor(private router: Router) {}
+
+    logout(): void {
+        const auth = getAuth();
+        signOut(auth)
+            .then(() => {
+                this.router.navigate(['/signin']);
+            })
+            .catch((error) => {
+                console.error('Error signing out: ', error);
+            });
+    }
 }
